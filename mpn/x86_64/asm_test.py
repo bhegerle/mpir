df82ff2c184dc051cad3dae129b266e96f4c1b1c
Blake Hegerle <blake.hegerle@allianzlife.com>
2020-09-23 15:41:23 -0500

    assembler feature tests

---
+++ mpn/x86_64/asm_test.py
@@ -0,0 +1,33 @@
+import sys
+import subprocess
+import re
+
+with open('.test.c', 'w') as c:
+    c.write('const int C=123; int I=1; const char *s="hi";')
+
+cmd = sys.argv[2:] + ['-S', '.test.c']
+run = subprocess.run(cmd)
+
+with open('.test.s', 'r') as s:
+    asm = s.read()
+
+def x(arg, pattern, func):
+    if sys.argv[1] == arg:
+        m = re.search(pattern, asm, re.M)
+        if m:
+            print(func(m))
+    elif sys.argv[1] == 'all':
+        m = re.search(pattern, asm, re.M)
+        print(f'{arg}: {func(m) if m else "?"}')
+    
+x('TEXT', r'^\s+(\.text)\b', lambda m: m[1])
+x('DATA', r'^\s+(\.data)\b', lambda m: m[1])
+x('LABEL_SUFFIX', r'^_?C(:)?', lambda m: m[1])
+x('GLOBL', r'^\s+(\.globl)\s+I\b', lambda m: m[1]) 
+x('GSYM_PREFIX', r'^(_?)C:?', lambda m: m[1])
+x('RODATA', r'^\s+(\.section)\b.*(\.rodata)', lambda m: m[1] + ' ' + m[2])
+x('TYPE', r'^\s+(\.type)\s+I\s*,\s*@\w+', lambda m: m[1] + ' $1,@$2')
+x('SIZE', r'^\s+(\.size)\s+I\s*,\s*\d+', lambda m: m[1] + ' $1,$2')
+x('LSYM_PREFIX', r'^\.L', lambda m: m[0])
+
+sys.exit(run.returncode)
