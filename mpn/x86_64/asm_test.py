ce43c04f98c232883c0931a1e9d332a6b13e679f
Blake Hegerle <bhegerle@gmail.com>
2020-09-27 09:57:58 -0500

    don't leave asm_test files in src tree

--- mpn/x86_64/asm_test.py
+++ mpn/x86_64/asm_test.py
@@ -1,7 +1,12 @@
 import sys
 import subprocess
 import re
-from os import listdir, remove
+import os
+
+if 'MESON_BUILD_ROOT' in os.environ:
+    test = os.path.join(os.environ['MESON_BUILD_ROOT'], '.test')
+else:
+    test = os.path.abspath('.test')
 
 
 cc = sys.argv[1:-1]
@@ -21,13 +26,13 @@ def test(mode_filter, gen, pattern, match_func):
 
 def execute(mode_filter, src, pattern, match_func):
     def gen():
-        with open('.test.c', 'w') as c:
+        with open(test + '.c', 'w') as c:
             c.write(src)
 
-        cmd = cc + ['-o', '.test', '.test.c']
+        cmd = cc + ['-o', test, test + '.c']
         subprocess.run(cmd, check=True)
 
-        run = subprocess.run('./.test', check=True, capture_output=True, text=True)
+        run = subprocess.run(test, check=True, capture_output=True, text=True)
         return run.stdout
 
     test(mode_filter, gen, pattern, match_func)
@@ -35,13 +40,13 @@ def execute(mode_filter, src, pattern, match_func):
         
 def parse_asm(mode_filter, pattern, match_func):
     def gen():
-        with open('.test.c', 'w') as c:
+        with open(test + '.c', 'w') as c:
             c.write('const int C=123; int I=1; const char *s="hi";')
 
-        cmd = cc + ['-S', '.test.c']
+        cmd = cc + ['-S', test + '.c']
         subprocess.run(cmd, check=True)
 
-        with open('.test.s', 'r') as s:
+        with open(test + '.s', 'r') as s:
             asm = s.read()
 
         return asm
@@ -53,29 +58,24 @@ def fail():
     raise Exception()
 
 
-try:
-    parse_asm('TEXT', r'^\s+(\.text\b|\.section\s+__TEXT.*)', lambda m: m[1])
-    parse_asm('DATA', r'^\s+(\.data\b|\.section\s+__DATA.*)', lambda m: m[1])
-    parse_asm('LABEL_SUFFIX', r'^_?C(:)?', lambda m: m[1])
-    parse_asm('GLOBL', r'^\s+(\.globl)\s+_?I\b', lambda m: m[1]) 
-    parse_asm('GSYM_PREFIX', r'^(_?)C:?', lambda m: m[1])
-    parse_asm('RODATA', r'^\s+(\.section)\s.*(\.rodata|__TEXT,__const)', lambda m: m[1] + ' ' + m[2])
-    parse_asm('TYPE', r'^\s+(\.type)\s+_?I\s*,\s*@\w+', lambda m: m[1] + ' $1,@$2' if m else '')
-    parse_asm('SIZE', r'^\s+(\.size)\s+_?I\s*,\s*\d+', lambda m: m[1] + ' $1,$2' if m else '')
-    parse_asm('LSYM_PREFIX', r'^\.?L', lambda m: m[0])
-    execute('ALIGN_LOGARITHMIC', r'''
-            #include <stdint.h>
-            #include <stdio.h>
-            int main() {
-                int64_t x, y;
-                asm(".align 4\nx:\n\t" "jmp y\n\t"
-                    ".align 4\ny:\n\t" "leaq x(%%rip), %0\n\t" "leaq y(%%rip), %1"
-                    : "=r"(x), "=r"(y) : : );
-                printf("%ld", y - x);
-                return 0;
-            }''',
-            r'\d+', lambda m: 'no' if m[0] == '4' else 'yes' if m[0] == '16' else fail())
-finally:
-    for d in listdir():
-        if d.startswith('.test'):
-            remove(d)
+parse_asm('TEXT', r'^\s+(\.text\b|\.section\s+__TEXT.*)', lambda m: m[1])
+parse_asm('DATA', r'^\s+(\.data\b|\.section\s+__DATA.*)', lambda m: m[1])
+parse_asm('LABEL_SUFFIX', r'^_?C(:)?', lambda m: m[1])
+parse_asm('GLOBL', r'^\s+(\.globl)\s+_?I\b', lambda m: m[1]) 
+parse_asm('GSYM_PREFIX', r'^(_?)C:?', lambda m: m[1])
+parse_asm('RODATA', r'^\s+(\.section)\s.*(\.rodata|__TEXT,__const)', lambda m: m[1] + ' ' + m[2])
+parse_asm('TYPE', r'^\s+(\.type)\s+_?I\s*,\s*@\w+', lambda m: m[1] + ' $1,@$2' if m else '')
+parse_asm('SIZE', r'^\s+(\.size)\s+_?I\s*,\s*\d+', lambda m: m[1] + ' $1,$2' if m else '')
+parse_asm('LSYM_PREFIX', r'^\.?L', lambda m: m[0])
+execute('ALIGN_LOGARITHMIC', r'''
+        #include <stdint.h>
+        #include <stdio.h>
+        int main() {
+            int64_t x, y;
+            asm(".align 4\nx:\n\t" "jmp y\n\t"
+                ".align 4\ny:\n\t" "leaq x(%%rip), %0\n\t" "leaq y(%%rip), %1"
+                : "=r"(x), "=r"(y) : : );
+            printf("%ld", y - x);
+            return 0;
+        }''',
+        r'\d+', lambda m: 'no' if m[0] == '4' else 'yes' if m[0] == '16' else fail())
